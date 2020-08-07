3e7ae0e805271f564cfbc7a1624f3da5d162710f
chiefsealion <49793405+chiefsealion@users.noreply.github.com>
2020-08-07 12:51:23 -0700

    Add files via upload

---
+++ PickFrom-Hat.ps1
@@ -0,0 +1,5 @@
+﻿$cred = Get-Credential
+
+$WinnerWinnerChickenDinner = Get-Random -InputObject (Get-Content "C:\Users\tcline\Desktop\Powershell Scripts\Pick-From-Hat.txt")
+
+Send-MailMessage -From 'tcline@isoutsource.com ' -To 'jthorson@isoutsource.com ', 'nchapman@isoutsource.com ' -Subject "Today's Winner is: $WinnerWinnerChickenDinner" -SmtpServer smtp.office365.com -UseSsl -Credential $cred -Port 587
