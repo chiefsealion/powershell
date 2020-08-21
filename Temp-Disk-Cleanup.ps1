8c98da1f2ae3141e3c6920b096081d52c2bfb8cb
chiefsealion <49793405+chiefsealion@users.noreply.github.com>
2020-08-21 13:51:19 -0700

    Update Temp-Disk-Cleanup.ps1

--- Temp-Disk-Cleanup.ps1
+++ Temp-Disk-Cleanup.ps1
@@ -1,29 +1,26 @@
-﻿#Set the path for each variable
+#Set variable to Retrieve the usernames for all users that have used this computer and store them in a txt file to be used for later
+$getUsers = (Get-ChildItem -Path C:\Users).Name | Out-File C:\Windows\LTSvc\Users.txt
+
+#Grab the list of each user that has ever signed into the computer
+$Users = Get-Content -Path C:\Windows\LTSvc\Users.txt
+
+#Set the path for each variable
 $pathTemp = "C:\Temp"
 $pathWindowsTemp = "C:\Windows\Temp"
-$pathTempIntFiles = "C:\Users\username\AppData\Local\Temporary Internet Files"
-$pathLocalTemp = "C:\Users\username\AppData\Local\Temp"
-$pathMicroTempIntFiles = "C:\Users\username\AppData\Microsoft\Windows\Temporary Internet Files"
+$pathTempIntFiles = "C:\Users\$User\AppData\Local\Temporary Internet Files"
+$pathLocalTemp = "C:\Users\$User\Appdata\Local\Temp"
+$pathMicroTempIntFiles = "C:\Users\$User\AppData\Microsoft\Windows\Temporary Internet Files"
+
+#Get size of Windows directories
+$Temp = "{0:N2} GB" -f ((gci $pathTemp | measure Length -s).Sum /1GB)
+$WindowsTemp = "{0:N2} GB" -f ((gci $pathWindowsTemp | measure Length -s).Sum /1GB)
 
 #Create txt file to store information for size of each directory
 New-Item -Path "C:\Windows\LTSvc\packages\DiskCleanupLogs" -Name "SizeofEachDirectory_PreScan.txt" -ItemType "File"
 
-#Check to see the size of the directories targeted for deletion
-$temp = "{0:N2} GB" -f ((gci $pathTemp | measure Length -s).Sum /1GB)
-$windowsTemp = "{0:N2} GB" -f ((gci $pathWindowsTemp | measure Length -s).Sum /1GB)
-$TempIntFiles = "{0:N2} GB" -f ((gci $pathTempIntFiles | measure Length -s).Sum /1GB)
-$LocalTemp = "{0:N2} GB" -f ((gci $pathLocalTemp | measure Length -s).Sum /1GB)
-$MicroTempIntFiles = "{0:N2} GB" -f ((gci $pathMicroTempIntFiles | measure Length -s).Sum /1GB)
-
 #Store variables just gathered for prescan results to correct directory
 Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathTemp is $temp"
 Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathWindowsTemp is $windowsTemp"
-Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathTempIntFiles is $TempIntFiles"
-Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathLocalTemp is $LocalTemp"
-Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathMicroTempIntFiles is $MicroTempIntFiles"
-
-#Grab the values just gathered for user to see
-Get-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt'
 
 #Force a cleaning of the Recycle Bin
 Clear-RecycleBin -Force
@@ -39,25 +36,65 @@ Remove-Item -Path $pathWindowsTemp -Include '*' -Recurse -WhatIf
 Stop-Transcript
 
 #Transcript the deletion of C:\Users\username\AppData\Local\Temporary Internet Files
+
 Start-Transcript -Path "C:\Windows\LTSvc\packages\DiskCleanupLogs\DiskClean_TempIntFiles.txt"
+
+foreach($User in $Users){
+
+$pathTempIntFiles = "C:\Users\$User\AppData\Local\Temporary Internet Files"
+$TempIntFiles = "{0:N2} GB" -f ((gci $pathTempIntFiles | measure Length -s).Sum /1GB)
+Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathTempIntFiles is $TempIntFiles"
 Remove-Item -Path $pathTempIntFiles -Include '*' -Recurse -WhatIf
+Remove-Item -Path $pathTempIntFiles -Include '*' -Recurse
+
+}
+
 Stop-Transcript
 
 #Transcript the deletion of C:\Users\username\AppData\Local\Temp
+
 Start-Transcript -Path "C:\Windows\LTSvc\packages\DiskCleanupLogs\DiskClean_LocalTemp.txt"
+
+foreach($User in $Users){
+
+$pathLocalTemp = "C:\Users\$User\Appdata\Local\Temp"
+$LocalTemp = "{0:N2} GB" -f ((gci $pathMicroTempIntFiles | measure Length -s).Sum /1GB)
+Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathLocalTemp is $LocalTemp"
 Remove-Item -Path $pathLocalTemp -Include '*' -Recurse -WhatIf
+Remove-Item -Path $pathLocalTemp -Include '*' -Recurse
+
+}
+
 Stop-Transcript
 
 #Transcript the deletion of C:\Users\username\AppData\Microsoft\Windows\Temporary Internet Files
+
 Start-Transcript -Path "C:\Windows\LTSvc\packages\DiskCleanupLogs\DiskClean_MicroTempFiles.txt"
-Remove-Item –path $pathMicroTempIntFiles -Include '*' -Recurse -WhatIf
+
+foreach($User in $Users){
+
+$pathMicroTempIntFiles = "C:\Users\$User\AppData\Microsoft\Windows\Temporary Internet Files"
+$MicroTempIntFiles = "{0:N2} GB" -f ((gci $pathMicroTempIntFiles | measure Length -s).Sum /1GB)
+Add-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Value "The size of the $pathMicroTempIntFiles is $MicroTempIntFiles"
+Remove-Item -Path $pathMicroTempIntFiles -Include '*' -Recurse -WhatIf
+Remove-Item -Path $pathMicroTempIntFiles -Include '*' -Recurse
+
+}
+
 Stop-Transcript
 
-#***ACTUALLY DELETING THE FILES HERE***
+#Grab the values just gathered for user to see
+Get-Content -Path 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt'
+Copy-Item 'C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt' -Destination 'C:\Windows\LTSvc' -Force
+
+
+#Deleting the Windows Temp Files Here
+
 Remove-Item -Path $pathTemp -Include '*' -Recurse
 Remove-Item -Path $pathWindowsTemp -Include '*' -Recurse
-Remove-Item -Path $pathTempIntFiles -Include '*' -Recurse
-Remove-Item -Path $pathLocalTemp -Include '*' -Recurse
-Remove-Item –path $pathMicroTempIntFiles -Include '*' -Recurse
 
+#Remove Size of Directory txt file for next use
+Remove-Item -Path "C:\Windows\LTSvc\packages\DiskCleanupLogs\SizeofEachDirectory_PreScan.txt"
+
+#Script Completion
 Write-Host "The process is complete. The directories have been cleaned successfully to your best permission level."
